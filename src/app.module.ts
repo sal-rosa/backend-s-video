dd1611016ceef4f3edad40da95e114d31f2899d6
sal-rosa <danielvasconcelos148@gmail.com>
2022-12-03 16:26:44 -0300

    first commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PostsModule } from './posts/posts.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [PostsModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
