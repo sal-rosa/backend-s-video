c50fd68990c466aa61d51d44ca5a2500730d0127
sal-rosa <danielvasconcelos148@gmail.com>
2022-12-04 22:23:55 -0300

    add create user and findone in db

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,19 +1,39 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/sequelize';
+import * as bcrypt from 'bcryptjs';
+
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 
+import { User } from './user.model';
+
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+
+  constructor(
+    @InjectModel(User)
+    private userModel: typeof User,
+  ) { }
+
+  async create(createUserDto: CreateUserDto): Promise<User> {
+    const hashedPassword = await bcrypt.hash(createUserDto.password, 10);
+    return this.userModel.create({
+      name: createUserDto.name,
+      username: createUserDto.username,
+      password: hashedPassword,
+    });
   }
 
   findAll() {
     return `This action returns all user`;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  findOne(username: string): Promise<User> {
+    return this.userModel.findOne({
+      where: {
+        username
+      },
+    });
   }
 
   update(id: number, updateUserDto: UpdateUserDto) {
